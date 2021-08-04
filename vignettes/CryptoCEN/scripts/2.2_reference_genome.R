e7b5714d3cea3249902e039b7209e2ce3a52b913
Matthew O'Meara <maom@gl-login2.arc-ts.umich.edu>
2021-08-04 14:23:49 -0400

    finish refactoring out prepare reference genome

--- vignettes/CryptoCEN/scripts/2.2_reference_genome.R
+++ vignettes/CryptoCEN/scripts/2.2_reference_genome.R
@@ -3,13 +3,4 @@ library(CalCEN)
 
 parameters <- CalCEN::load_parameters()
 
-
-cmd <- paste0(
-  parameters$software_paths$rsem_path, "/rsem-prepare-reference \\
-  --num-threads 10 \\
-  --bowtie2 --bowtie2-path ", parameters$software_paths$bowtie2_path, " \\
-  ", parameters$source_data$genome$transcript_fasta_path, " \\
-  ", parameters$data_paths$reference_genome_path)
-cat(cmd, "\n", sep = "")
-
-system(cmd)
+CalCEN::prepare_reference_genome(parameters)
